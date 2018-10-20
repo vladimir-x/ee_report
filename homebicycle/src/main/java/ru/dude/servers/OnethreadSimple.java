f13ae5b77b8522c4c2e6e248127c3294b4d04273
dude <vladimir.x89@mail.ru>
2018-10-20 12:18:50 +0300

    singlethread fix

--- homebicycle/src/main/java/ru/dude/servers/OnethreadSimple.java
+++ homebicycle/src/main/java/ru/dude/servers/OnethreadSimple.java
@@ -1,23 +1,19 @@
 package ru.dude.servers;
 
-import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class OnethreadSimple {
 
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         ServerSocket ss = new ServerSocket(8080);
         while (true) {
             Socket s = ss.accept();
             System.err.println("Client accepted");
-            s.getOutputStream().write("<html/>".getBytes());
-            s.getOutputStream().flush();
-            //s.getChannel().
-
-           // s.
-           // new Thread(new SocketProcessor(s)).start();
+            String head = "HTTP/1.1 200 OK\n Connection: close\n\n";
+            String resp = "<html><body><h1>OnethreadSimple hello</h1></body></html>";
+            s.getOutputStream().write((head + resp).getBytes());
+            s.close();
         }
     }
 }
